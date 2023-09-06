549c1619e81b725af81900047c7f38a296c469cd
chiwei-ouob <144209904+chiwei-ouob@users.noreply.github.com>
2023-09-06 16:11:46 +0800

    Add files via upload

---
+++ BMI.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+w = float(input("Enter your weight (kilogram): "))
+h = float(input("Enter your height (meter): "))
+bmi = w/(h**2)
+print (f"Your BMI is: {bmi:.2f}")
